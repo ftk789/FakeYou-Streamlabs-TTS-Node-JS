f8b4dea7ef516bb804e7f76841b0a4944c808746
moath alayel <57598907+ftk789@users.noreply.github.com>
2022-05-05 02:15:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const { randomBytes } = require("crypto");
 const { pipeline } = require("stream");
 const { promisify } = require("util");
 const { unlink } = require("fs");
-
+var voice = 'TM:7wbtjphx8h8v';
 const { requestSpeech, getVoiceList } = require("C:\\Users\\lolt0\\Desktop\\Streamlabs test project\\fakeYou.js");
-const { voice } = '2pac'
 const { fakeYouToken } = 'SAPI:FTK789_6096133E1D79D6FB'
  
   const streamlabs = io(`https://sockets.streamlabs.com?token=${socketToken}`, {transports: ['websocket']});
@@ -28,6 +27,48 @@ const { fakeYouToken } = 'SAPI:FTK789_6096133E1D79D6FB'
       console.log('hi');
      
     }
+	if(eventData.for === 'twitch_account' && eventData.type == 'subscription'){
+		var donobj = eventData.message;
+		var dononame = donobj[0].name;
+		var donoamo = donobj[0].amount;
+		var donomessage = donobj[0].message;
+		var message = donomessage;
+		console.log(message);
+		console.log(voice)
+		function requestSpeechFile(voice, message) {
+			return new Promise(async(resolve, reject) => {
+				// Launch speech request and poll until completion
+				const url = await requestSpeech(voice, message).catch(reject);
+				if (!url) return;
+				const response = await fetch(url).catch(error => {
+					reject(`HTTP error! ${error.name}`);
+					console.error(error);
+				});
+				if (!response.ok) return;
+		
+				// Generate random temporary filename to avoid overwriting other recordings
+				const filePath = `./${randomBytes(48).toString("hex")}.wav`;
+		
+				const streamPipeline = promisify(pipeline);
+				await streamPipeline(response.body, createWriteStream(filePath)).then(() => {
+		
+					resolve(filePath);
+				}).catch(error => {
+					if (!error) return;
+					reject("Failed to write file!");
+					console.error(error);
+				});
+			});
+		}
+		
+		
+		
+				requestSpeechFile(voice, message).then(async(filePath) => {
+		console.log('its working')
+					// Send temporary file in new message to avoid 15 minute interaction expiry time
+					}).catch(console.error);
+
+	}
     if(eventData.for === 'streamlabs' && eventData.type == 'donation'){
       var donobj = eventData.message;
       var dononame = donobj[0].name;
@@ -35,7 +76,6 @@ const { fakeYouToken } = 'SAPI:FTK789_6096133E1D79D6FB'
       var donomessage = donobj[0].message;
 	  var message = donomessage;
       console.log(message);
-      var voice = '2pac';
 	  console.log(voice)
 	
 function requestSpeechFile(voice, message) {
